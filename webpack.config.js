03411ff23adf8c6b7a5cc8cae94b39922f2d564f
Anatoly U <korvet07@yandex.ru>
2022-12-12 16:25:08 +0300

    Время разбивать камни

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,16 @@ module.exports = {
     }),
   ],
   module: {
-    rules: [{
+    rules: [
+      {
       test: /\.js$/,
       exclude: /(node_modules)/,
       use: ['babel-loader']
-    }]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+  ]
   },
 };
