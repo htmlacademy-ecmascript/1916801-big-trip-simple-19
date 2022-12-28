d04299db65ceb81542fedd533d9aaa6e0decc46c
Anatoly U <korvet07@yandex.ru>
2022-12-28 09:54:35 +0300

    Революция или эволюция? (часть 2)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './views/filter-view';
+import FilterView from './views/filter-view';
 import './views/sort-view';
 import ListView from './views/list-view';
 import './views/point-view';
@@ -12,6 +12,7 @@ import DestinationAdapter from './adapters/destination-adapter';
 import { FilterType, SortType } from './enums';
 import { filterCallbackMap, sortCallbackMap } from './maps';
 import ListPresenter from './presenters/list-presenter';
+import FilterPresenter from './presenters/filter-presenter';
 
 const BASE = 'https://19.ecmascript.pages.academy/big-trip-simple';
 const AUTH = 'Basic stringAnatoly070';
@@ -23,7 +24,7 @@ const pointsStore = new Store(`${BASE}/points`, AUTH);
 const pointsModel = new CollectionModel({
   store: pointsStore,
   adapt: (item) => new PointAdapter(item),
-  filter: filterCallbackMap[FilterType.FUTURE],
+  filter: filterCallbackMap[FilterType.EVERYTHING],
   sort: sortCallbackMap[SortType.DAY]
 
 });
@@ -48,6 +49,7 @@ const offerGroupsModel = new CollectionModel({
 });
 const models = [pointsModel, destinationsModel, offerGroupsModel];
 const listView = document.querySelector(String(ListView));
+const filterView = document.querySelector(String(FilterView));
 
 const { log } = console;
 
@@ -55,6 +57,7 @@ Promise.all(
   models.map((model) => model.ready())
 )
   .then(async () => {
+    new FilterPresenter(filterView, models);
     new ListPresenter(listView, models);
   })
 
--- src/views/sort-view.js
+++ src/views/sort-view.js
@@ -13,7 +13,7 @@ export default class SortView extends View {
     * @override
     */
   createHtml() {
-    return html`
+    return html/*html*/`
     <div class="trip-sort__item  trip-sort__item--day">
       <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
       <label class="trip-sort__btn" for="sort-day">Day</label>
