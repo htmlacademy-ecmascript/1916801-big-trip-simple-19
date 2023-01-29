31ff4bb5b1aab180732d09f35a19ddaf05102ac6
Anatoly U <korvet07@yandex.ru>
2023-01-29 19:16:27 +0300

    6.7. Обновление века (часть 1)

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import FilterView from './views/filter-view';
-import './views/sort-view';
 import ListView from './views/list-view';
-import './views/point-view';
 import SortView from './views/sort-view';
 import NewPointEditorView from './views/new-point-editor-view';
 import PointEditorView from './views/point-editor-view';
 
 import Store from './store';
+
 import CollectionModel from './models/collection-model';
 import PointAdapter from './adapters/point-adapter';
-import OfferGroupAdapter from './adapters/offer-group-adapter';
 import DestinationAdapter from './adapters/destination-adapter';
+import OfferGroupAdapter from './adapters/offer-group-adapter';
+
 import { FilterType, SortType } from './enums';
 import { filterCallbackMap, sortCallbackMap } from './maps';
+
 import ListPresenter from './presenters/list-presenter';
 import FilterPresenter from './presenters/filter-presenter';
 import SortPresenter from './presenters/sort-presenter';
@@ -23,23 +24,22 @@ import EmptyListPresenter from './presenters/empty-list-presenter';
 
 
 const BASE = 'https://19.ecmascript.pages.academy/big-trip-simple';
-const AUTH = 'Basic stringAnatoly070';
+const AUTH = 'Basic korvet07ZZ0';
 
 /**
- * @type {Store <Point>}
- */
+ * @type {Store<Point>}
+*/
 const pointsStore = new Store(`${BASE}/points`, AUTH);
 const pointsModel = new CollectionModel({
   store: pointsStore,
   adapt: (item) => new PointAdapter(item),
   filter: filterCallbackMap[FilterType.EVERYTHING],
   sort: sortCallbackMap[SortType.DAY]
-
 });
 
 /**
- * @type {Store <Destination>}
- */
+ * @type {Store<Destination>}
+*/
 const destinationsStore = new Store(`${BASE}/destinations`, AUTH);
 const destinationsModel = new CollectionModel({
   store: destinationsStore,
@@ -47,37 +47,37 @@ const destinationsModel = new CollectionModel({
 });
 
 /**
- * @type {Store <OfferGroup>}
- */
+ * @type {Store<OfferGroup>}
+*/
 const offerGroupsStore = new Store(`${BASE}/offers`, AUTH);
 const offerGroupsModel = new CollectionModel({
   store: offerGroupsStore,
   adapt: (item) => new OfferGroupAdapter(item)
 });
+
 const models = [pointsModel, destinationsModel, offerGroupsModel];
+
 const listView = document.querySelector(String(ListView));
-const emptyListView = document.querySelector('.trip-events__msg');
 const filterView = document.querySelector(String(FilterView));
 const sortView = document.querySelector(String(SortView));
 const newPointButtonView = document.querySelector('.trip-main__event-add-btn');
 const newPointEditorView = new NewPointEditorView(listView);
 const pointEditorView = new PointEditorView(listView);
-
-const { log } = console;
+const emptyListView = document.querySelector('.trip-events__msg');
 
 Promise.all(
   models.map((model) => model.ready())
 )
-  .then(async () => {
+  .then(() => {
     new FilterPresenter(filterView, models);
     new SortPresenter(sortView, models);
     new ListPresenter(listView, models);
-    new EmptyListPresenter(emptyListView, models);
     new NewPointButtonPresenter(newPointButtonView, models);
     new NewPointEditorPresenter(newPointEditorView, models);
     new PointEditorPresenter(pointEditorView, models);
+    new EmptyListPresenter(emptyListView, models);
   })
 
-  .catch((error) => {
-    log(error);
+  .catch((exception) => {
+    emptyListView.textContent = exception;
   });
